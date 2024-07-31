d21a1ee0aa9a5e1c76702a1a7882588c16121765
Brett Czerwinski <brettcz@outlook.com>
2024-07-30 18:57:31 -0600

    edited resolvers, changed mutation name for adding a user

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -33,7 +33,7 @@ const typeDefs = `
   }
 
   type Mutation {
-    createUser(username: String!, email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
     saveBook(input: BookData): User
     removeBook(bookId: String!): User
